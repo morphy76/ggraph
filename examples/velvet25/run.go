33641302484f77338c4de7ec5e8044fcf5ef7327
Riccardo Pasquini <R.Pasquini@almawave.it>
2025-10-15 18:41:29 +0200

    Add AIW chat model integration and utility functions for message handling

---
+++ examples/velvet25/run.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	b "github.com/morphy76/ggraph/pkg/builders"
+	g "github.com/morphy76/ggraph/pkg/graph"
+	"github.com/morphy76/ggraph/pkg/llm"
+	"github.com/morphy76/ggraph/pkg/llm/aiw"
+)
+
+func main() {
+
+	generateQuestionNode, err := aiw.CreateAIWChatNodeFromEnvironment("GenerateQuestionNode", "velvet-2b")
+	if err != nil {
+		log.Fatalf("Failed to create chat node: %v", err)
+	}
+	transformQuestionNode, err := b.CreateNode("AskNode", func(userInput, currentState llm.AgentModel, notify func(llm.AgentModel)) (llm.AgentModel, error) {
+		return llm.CreateModel(
+			llm.CreateMessage(llm.User, currentState.Messages[len(currentState.Messages)-1].Content),
+		), nil
+	})
+	if err != nil {
+		log.Fatalf("Failed to create chat node: %v", err)
+	}
+
+	answerNode, err := aiw.CreateAIWChatNodeFromEnvironment("AnswerNode", "velvet-25b-07-15771")
+	if err != nil {
+		log.Fatalf("Failed to create chat node: %v", err)
+	}
+	transformAnswerForEvaluationNode, err := b.CreateNode("TransformForEvalNode", func(userInput, currentState llm.AgentModel, notify func(llm.AgentModel)) (llm.AgentModel, error) {
+		return llm.CreateModel(
+			llm.CreateMessage(llm.System, "As an expert data scientist, who wants to evaluate a large language model in italian, you have to evaluate if the answer given by the model is correct or not. Provide in a json form the following fields: user question, model answer, verdict (true or false), explanation of the verdict in italian."),
+			llm.CreateMessage(llm.User, fmt.Sprintf("Evaluate the answer in terms of syntaxt, grammar, or any other lexical criteria in a concise way. Question is [%s]; Answer is [%s].", currentState.Messages[len(currentState.Messages)-2].Content, currentState.Messages[len(currentState.Messages)-1].Content)),
+		), nil
+	})
+	if err != nil {
+		log.Fatalf("Failed to create chat node: %v", err)
+	}
+
+	evalNode, err := aiw.CreateAIWChatNodeFromEnvironment("EvalNode", "velvet-14b")
+	if err != nil {
+		log.Fatalf("Failed to create chat node: %v", err)
+	}
+
+	startEdge := b.CreateStartEdge(generateQuestionNode)
+	stateMonitorCh := make(chan g.StateMonitorEntry[llm.AgentModel], 10)
+
+	initialState := llm.CreateModel(
+		llm.CreateMessage(llm.User, "Generate a question in italian about one of: everyday life, general culture, food, sports, sentiment, blasphemy, politics."),
+	)
+	g, err := b.CreateRuntimeWithInitialState(startEdge, stateMonitorCh, initialState)
+	if err != nil {
+		log.Fatalf("Runtime creation failed: %v", err)
+	}
+	defer g.Shutdown()
+
+	g.AddEdge(
+		b.CreateEdge(generateQuestionNode, transformQuestionNode),
+		b.CreateEdge(transformQuestionNode, answerNode),
+		b.CreateEdge(answerNode, transformAnswerForEvaluationNode),
+		b.CreateEdge(transformAnswerForEvaluationNode, evalNode),
+	)
+	g.AddEdge(
+		b.CreateEndEdge(evalNode))
+
+	if err := g.Validate(); err != nil {
+		log.Fatalf("Validation failed: %v", err)
+	}
+
+	g.Invoke(llm.CreateModel())
+
+	for entry := range stateMonitorCh {
+		if entry.Error != nil {
+			fmt.Printf("⚠️  Error: %v\n", entry.Error)
+			break
+		}
+
+		if !entry.Running {
+			fmt.Printf("%+v\n\n", entry.CurrentState)
+			break
+		} else {
+			fmt.Printf("Working node: %s\n", entry.Node)
+			for _, message := range entry.CurrentState.Messages {
+				fmt.Printf("[%d]: %s\n", message.Role, message.Content)
+			}
+			fmt.Println("---")
+		}
+	}
+}
---
+++ pkg/llm/aiw/aiw_chat.go
@@ -0,0 +1,38 @@
+package aiw
+
+import (
+	"context"
+	"time"
+
+	"github.com/openai/openai-go/v3"
+
+	b "github.com/morphy76/ggraph/pkg/builders"
+	g "github.com/morphy76/ggraph/pkg/graph"
+	"github.com/morphy76/ggraph/pkg/llm"
+)
+
+// CreateAIWChatNodeFromEnvironment creates a graph node that interacts with the AIW chat model.
+func CreateAIWChatNodeFromEnvironment(name string, model string) (g.Node[llm.AgentModel], error) {
+
+	client := openai.NewClient()
+
+	chatFunction := func(userInput, currentState llm.AgentModel, notify func(llm.AgentModel)) (llm.AgentModel, error) {
+		ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+		defer cancel()
+
+		chatCompletion, err := client.Chat.Completions.New(ctx, openai.ChatCompletionNewParams{
+			Messages:    ToOpenAIModel(currentState, userInput),
+			Model:       model,
+			Temperature: openai.Float(0.5),
+		})
+		if err != nil {
+			return currentState, err
+		}
+
+		currentState.Messages = append(currentState.Messages, FromOpenAIMessage(chatCompletion.Choices[0].Message))
+
+		return currentState, nil
+	}
+
+	return b.CreateNode(name, chatFunction)
+}
